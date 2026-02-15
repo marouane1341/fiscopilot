8c78fd334877402fe680254270bd09a54c87dce1
marouane1341 <marouane1341@gmail.com>
2026-02-15 23:37:07 +0100

    Add files via upload

---
+++ knowledge.js
@@ -0,0 +1,51 @@
+const knowledge = [
+
+{
+keywords: ["tva"],
+answer:
+"La TVA standard en Belgique est 21%. Les taux réduits sont 6% et 12%."
+},
+
+{
+keywords: ["isoc"],
+answer:
+"L'impôt des sociétés en Belgique est de 25%."
+},
+
+{
+keywords: ["ipp"],
+answer:
+"L'impôt des personnes physiques est progressif en Belgique."
+},
+
+{
+keywords: ["pcmn"],
+answer:
+"Le PCMN est le Plan Comptable Minimum Normalisé."
+},
+
+{
+keywords: ["ifrs"],
+answer:
+"Les IFRS sont des normes comptables internationales."
+},
+
+{
+keywords: ["amortissement"],
+answer:
+"L'amortissement répartit la valeur d'un actif sur sa durée de vie."
+},
+
+{
+keywords: ["blanchiment"],
+answer:
+"La loi anti-blanchiment impose la déclaration des opérations suspectes."
+},
+
+{
+keywords: ["societe"],
+answer:
+"Les sociétés belges sont régies par le Code des Sociétés et Associations."
+}
+
+];
